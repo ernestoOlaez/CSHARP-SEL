e37eb7f4c0df136327970c9db93df34d198b2a11
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2022-03-21 18:08:14 -0600

    Add code for Maximize the Window

---
+++ CSHARP-SEL/CSHARP-SEL/IWebDriverCommands/FirstTestCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace CSHARP_SEL.IWebDriverCommands
+{
+    internal class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+
+            //Launch the WebBrowser and Maximize the Window.
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/";
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
